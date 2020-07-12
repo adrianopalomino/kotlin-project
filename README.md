03e8b2be5f5842fdc8bc7b639ee2f9cb7166e3c4
adriano.dantas <adriano.dantas@ctis.com.br>
2020-07-12 20:43:16 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Getting Started
+
+### Plugin Setup
+
+https://dl.bintray.com/jetbrains/kotlin/eclipse-plugin/0.8.19/
+
+### Reference Documentation
+
+https://spring.io/guides/tutorials/spring-boot-kotlin/
+
+### Running project
+
+mvn spring-boot:run
+
---
+++ src/main/kotlin/com/example/kotlinproject/HtmlController.kt
@@ -0,0 +1,17 @@
+package com.example.kotlinproject
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.ui.set
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class HtmlController {
+
+	@GetMapping("/")
+	fun kotlinproject(model: Model): String {
+		model["title"] = "kotlinproject"
+		return "kotlinproject"
+	}
+
+}
